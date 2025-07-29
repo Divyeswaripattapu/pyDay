ca8b2d20bfb6965934dee4f9aae2169b003cb321
PATTAPU DIVYESWARI <22ht1a05b3@gmail.com>
2025-07-29 20:35:42 +0530

    second commit

---
+++ Day2/que1.py
@@ -0,0 +1,8 @@
+# Declare two variables a and b, assign integer values to them, and print their sum
+
+
+a = int(input("Enter the your number : "))
+
+b = int(input("Enter the your another number : "))
+
+print("The sum of a and b is :" +str(a+b))
